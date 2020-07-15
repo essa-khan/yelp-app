f809acf1bb8027732a72b2528dc43fbedfb05475
Essa Khan <eskhansa@gmail.com>
2020-07-14 20:03:17 -0500

    Display search_businesses results in browser

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import yelp
 
 app = Flask(__name__)
 
@@ -7,7 +8,19 @@ app = Flask(__name__)
 def index():
     #Return a page
     return render_template("index.html")
+
+@app.route("/submit")
+def submit():
+    #Submission page
+    user_submitted_term = request.values.get("term")
+    user_submitted_location = request.values.get("location")
+    
+    results = yelp.search_businesses(user_submitted_term, user_submitted_location)
+    return results
         
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 my_port = 5000
 
 app.run(host="0.0.0.0", port=my_port, debug=True)
+
